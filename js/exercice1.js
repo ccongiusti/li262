3dc5a5df83d049cbce8a545577ae621ae5e54f5c
Christophe CONGIUSTI <ccongiusti@free.fr>
2018-02-26 14:21:34 +0100

    ok

--- js/exercice1.js
+++ js/exercice1.js
@@ -55,5 +55,44 @@ console.log(client.commandes);
 client.lunettes = true;
 console.log(client);
 
+// Tableau d'objets
+// Objets en attr
+let pere = { "prenom":"Christophe", "nom":"CONGIUSTI" };
+let enfants = [{ "prenom":"Enzo", "nom":"CONGIUSTI" }, { "prenom":"Mathis", "nom":"CONGIUSTI" }];
+pere.enfants = enfants;
+console.log(pere);
 
+// Tableaux
+let tabEnfants = [];
+const enfant1 = { "prenom":"Enzo", "nom":"CONGIUSTI" };
+const enfant2 = { "prenom":"Mathis", "nom":"CONGIUSTI" };
+tabEnfants.push(enfant1);
+tabEnfants.push(enfant2);
+console.log(tabEnfants);
 
+// Boucle for
+for (let i=0;i<tabEnfants.length;i++) {
+    console.log("enfant["+i+"]="+tabEnfants[i].prenom);
+}
+
+// Boucle while ou do while
+// while () {}
+// do {} while ();
+
+// Operateur
+let j=0;
+console.log(j++);
+console.log(j);
+console.log(++j);
+j*=2;
+console.log(j);
+
+// Switch/case
+let sw = "2";
+switch (sw) {
+    case "2" :
+        console.log("Case \"2\""); 
+        break;
+    default:
+        console.log("Par defaut"); 
+}
--- js/exercice2.js
+++ js/exercice2.js
@@ -1,5 +1,5 @@
 function bonjour() {
-    console.log("bonjour");
+    console.log("Bonjour !");
 }
 bonjour();
 
@@ -8,3 +8,48 @@ function affSomme(a, b) {
 }
 affSomme(2,3); // affiche 5
 affSomme(2,"3"); // affiche 23
+
+function somme(a, b) {
+    return a+b;
+}
+console.log(somme(3,4)); // affiche 7
+
+let sommeVar = function(a, b) {
+    return a+b;
+};
+console.log(sommeVar(5,4)); // affiche 9
+
+let ditBonjour = function(a, b) {
+    console.log("Bonjour !");
+};
+let longueFonction = function(maFonction, delais) {
+    if (delais>0) {
+        console.log("lancement de '"+maFonction.name+"'");
+        setTimeout(() => { 
+            maFonction();
+        }, delais);
+        console.log("lancement apres timeout "+delais+"ms...");
+    } else {
+        console.log("lancement de sans timeout de '"+maFonction.name+"'");
+        maFonction();
+    }    
+};
+longueFonction(ditBonjour, 0);
+
+let client = {};
+client.nom = "CONGIUSTI";
+client.prenom = "Christophe";
+client.ditBonjour = ditBonjour;
+client.ditBonjour();
+
+client.commandes = [ 123, 456, 222 ];
+client.getNbCommandes = function () {
+    return this.commandes.length;
+};
+console.log("getNbCommandes() retourne "+client.getNbCommandes());
+
+client.ajoutCommande = function (nouvelleCommande) {
+    return this.commandes.push(nouvelleCommande);
+};
+client.ajoutCommande(666);
+console.log("getNbCommandes() retourne "+client.getNbCommandes());
